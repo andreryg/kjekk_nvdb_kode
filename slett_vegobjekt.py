883d3349adecc214dc11c1ef2460c8f9081ff28a
Andreas Gangstad Rygg <andreas.gangstad.rygg@vegvesen.no>
2024-07-08 12:43:48 +0200

    Laget fil for sletting av vegobjekt

---
+++ slett_vegobjekt.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 12:15:11 2024
+
+@author: andryg
+"""
+
+def slett_vegobjekt(id):
+    """
+
+    Parameters
+    ----------
+    id : int
+        Vegobjekt id til objekt som skal slettes.
+
+    Returns
+    -------
+    bool
+        Returnerer True hvis vegobjekt ble suksessfullt slettet. 
+
+    """
+    return True
